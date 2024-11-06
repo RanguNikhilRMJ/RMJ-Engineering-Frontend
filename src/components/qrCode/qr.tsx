f46b7b355a1c0657bcbc713b627f91dde768f4a6
SandeepReddyRmj <sandeep@rmjit.com>
2024-11-06 16:30:59 +0530

    error solved changes
    error solved changes

--- src/components/qrCode/qr.tsx
+++ src/components/qrCode/qr.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import Barcode from 'react-barcode';
 
-function QrCodeComponent({ data }) {
-  // Extract the name and code from the input data
+// Define the type for each item in the data array
+interface DataItem {
+  name: string;
+  code: string;
+}
+
+// Define the component props using the DataItem type
+interface QrCodeComponentProps {
+  data: DataItem[];
+}
+
+const QrCodeComponent: React.FC<QrCodeComponentProps> = ({ data }) => {
   return (
-    <>
-    <div style={{ display: "flex", gap: "20px",flexWrap:"wrap" }}>
+    <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
       {data.map((item, index) => (
         <div key={index}>
           <h2>{item.name}</h2>
-          <div >
-          <Barcode value={item.code}
-           width={1} 
-           height={50}  
-          />
+          <div>
+            <Barcode value={item.code} width={1} height={50} />
           </div>
         </div>
       ))}
     </div>
-  </>
-    );
-}
+  );
+};
 
 export default QrCodeComponent;
